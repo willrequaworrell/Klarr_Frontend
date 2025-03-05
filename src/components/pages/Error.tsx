7782ab703502dee585b4616cf8a7414788c95e88
Will Worrell <willrequaworrell@gmail.com>
2025-03-04 20:35:15 -0500

    updated router error page

--- src/components/pages/Error.tsx
+++ src/components/pages/Error.tsx
@@ -1,11 +1,23 @@
+import Background from "../Background"
 
 
 const Error = () => {
-    // const error = useRouteError() 
     return (
-        <div>
-            <p>Whoops! Looks like we can't find what you're looking for</p>
-        </div>
+        <Background>
+            <div className="flex flex-col items-center justify-center min-h-screen">
+                <div className="flex items-center justify-center">
+                    <img 
+                        src="klarr.png" 
+                        alt="Klarr Logo - overlapping black and white circles & rectangles" 
+                        className="object-cover object-center h-[7vh]"
+                    />
+                    <h1 aria-label="Klarr Logo Text" className="tracking-wider text-[6vh] text-offblack font-Staat">
+                        KLARR
+                    </h1>
+                </div>
+                <p className="font-bold text-offblack text-[2vw]">Whoops! Looks like we can't find what you're looking for</p>
+            </div>
+        </Background>
     )
 }
 
